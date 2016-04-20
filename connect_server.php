05f07bc4d7096a0fa251ea0668a95ba1f5a010ae
Bao Tran <btran12@users.noreply.github.com>
2016-04-20 19:44:12 -0400

    Update connect_server.php

--- connect_server.php
+++ connect_server.php
@@ -1,12 +1,12 @@
-<?php
-//connection parameters
-$dbtype = "mysql";
-$dbhost = "globeotter.com";
-$dbname = "ndgreen_360";
-$dbuser = "ndgreen_student";
-$dbpass = "G6wCn2*VV=[4";
-
-//set up connection
-$conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-
-?>
+ +<?php
+ +//connection parameters
+ +$dbtype = "mysql";
+ +$dbhost = "host";
+ +$dbname = "dbname";
+ +$dbuser = "dbuser";
+ +$dbpass = "dbpass";
+ +
+ +//set up connection
+ +$conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+ +
+ +?>
